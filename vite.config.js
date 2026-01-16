33832409490aaf94fdfc0a40a1ac817c586d0bd7
rahmoundif <rahmoun.dif@gmail.com>
2026-01-16 23:18:04 +0100

    feat/init VITE PWA testing

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
+	plugins: [
+		react(), 
+		tailwindcss(), 
+		VitePWA({ 
+			registerType: 'autoUpdate' 
+		})],
 });
